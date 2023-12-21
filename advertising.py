eb31e071ba0f18baf9430dbfafbdb739177895ce
roseainashazleen <154338276+roseainashazleen@users.noreply.github.com>
2023-12-21 16:00:02 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -9,12 +9,12 @@ st.write("This app predicts the **Sales** !")
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    Tv_Sales = st.sidebar.slider('Tv Sales', 0.7, 296.4, 5.4)
-    Radio_Sales = st.sidebar.slider('Radio Sales', 0.0, 49.6, 3.4)
-    Newspaper_Sales = st.sidebar.slider('Newspaper Sales', 0.3, 114.0, 1.3)
-    data = {'Tv': Tv_Sales,
-            'Radio': Radio_Sales,
-            'Newspaper': Newspaper_Sales}
+    Tv = st.sidebar.slider('Tv', 0.7, 296.4, 5.4)
+    Radio = st.sidebar.slider('Radio', 0.0, 49.6, 3.4)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 1.3)
+    data = {'Tv': Tv,
+            'Radio': Radio,
+            'Newspaper': Newspaper}
     features = pd.DataFrame(data, index=[0])
     return features
 
